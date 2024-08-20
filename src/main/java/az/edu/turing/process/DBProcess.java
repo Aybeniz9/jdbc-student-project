76a46cc654106ae09a471b9533b0d8606227e816
aybeniz <aybenizkazimova15@gmail.com>
2024-08-20 23:57:01 +0400

    Student JDBC project

--- src/main/java/az/edu/turing/process/DBProcess.java
+++ src/main/java/az/edu/turing/process/DBProcess.java
@@ -89,7 +89,6 @@ public class DBProcess {
             }
         }
     }
-
     public static void deleteStudent(Integer studentId) {
         String query = "DELETE FROM student WHERE studentId=?";
         try {
